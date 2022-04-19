3c83045e5a2eb64666e2945790af4071d7917f38
adisidev <64905594+adisidev@users.noreply.github.com>
2022-04-19 16:35:46 +0800

    Added perspective point at origin, and basic perspective drawing

--- pyparus.py
+++ pyparus.py
@@ -1,73 +1,64 @@
 import turtle
+import math
 
 # With inspiration from:
 # https://www.geeksforgeeks.org/python-turtle-graphics-keyboard-commands/
 
-# turtle.setup(500, 500)
+# Initial setup
 turtle.Screen()
 t = turtle.Turtle()
+
+# No delay
 t.speed(0)
-t.width(5)
-# turtle.showturtle()
 
+block_size = 100.0
 
-def up():
-    t.setheading(90)
-    t.forward(100)
+# Movement functions
+def set_heading_and_move(direction, distance = block_size):
+    t.setheading(direction)
+    t.fd(distance)
 
+def up(): set_heading_and_move(90.0)
+def down(): set_heading_and_move(270.0)
+def left(): set_heading_and_move(180.0)
+def right(): set_heading_and_move(0.0)
 
-def down():
-    t.setheading(270)
-    t.forward(100)
-
-
-def left():
-    t.setheading(180)
-    t.forward(100)
-
-
-def right():
-    t.setheading(0)
-    t.forward(100)
-
-
-def r():
-    t.color('red')
-
-
-def g():
-    t.color('green')
-
-def u():
-    t.undo()
-
-
-def b():
-    t.color('blue')
-
-
-def z():
-    t.color('black')
-
-def q():
-    turtle.bye()
-
+# Move toward perspective
+def perspective(direction = 1.0):
+    angle = t.towards(0.0, 0.0)
+    distance = abs(block_size / math.cos(math.radians(angle)))
+    set_heading_and_move(angle, distance * direction)
 
 turtle.listen()
+
+# Movement
+turtle.onkey(up, 'w')
+turtle.onkey(down, 's')
+turtle.onkey(left, 'a')
+turtle.onkey(right, 'd')
+
+# Arrow keys
 turtle.onkey(up, 'Up')
 turtle.onkey(down, 'Down')
 turtle.onkey(left, 'Left')
 turtle.onkey(right, 'Right')
-turtle.onkey(z, 'z')
+
+# Move to perspective point
+turtle.onkey(perspective, 'j')
+
+# Move away perspective point
+turtle.onkey(lambda: perspective(-1.0), 'k')
 
 # Quit
-turtle.onkey(q, 'q')
+turtle.onkey(turtle.bye, 'q')
 
 # Undo
-turtle.onkey(u, 'u')
+turtle.onkey(lambda: turtle, 'u')
 
-turtle.onkey(r, 'r')
-turtle.onkey(g, 'g')
-turtle.onkey(b, 'b')
+# Colors
+# turtle.onkey(lambda: t.color('red'), 'r')
+# turtle.onkey(lambda: t.color('green'), 'g')
+# turtle.onkey(lambda: t.color('blue'), 'b')
+# turtle.onkey(lambda: t.color('black'), 'r')
 
 turtle.mainloop()
